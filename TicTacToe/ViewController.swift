aba08815190b296df91a3438e432b9fea700f7b8
Oğulcan Aslan <batinsavran00@gmail.com>
2024-09-20 17:24:12 +0300

    Add Board Reset Functionality and Draw Condition to Tic-Tac-Toe
    Initialized the game board buttons into an array for easier management.
    Implemented fullBoard() function to check for a draw condition when the board is full.
    Added resultAlert() to show an action sheet for resetting the board after a game ends in a draw.
    Created a resetBoard() function to clear the board and reset the game state.

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -25,15 +25,69 @@ class ViewController: UIViewController
     
     var NOUGHT = "O"
     var CROSS = "X"
+    var board = [UIButton]()
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        initBoard()
+    }
+    
+    func initBoard()
+    {
+        board.append(a1)
+        board.append(a2)
+        board.append(a3)
+        board.append(b1)
+        board.append(b2)
+        board.append(b3)
+        board.append(c1)
+        board.append(c2)
+        board.append(c3)
     }
     
     @IBAction func boardTapAction(_ sender: UIButton)
     {
         addToBoard(sender)
+        
+        if(fullBoard()) {
+            resultAlert(tittle: "Draw")
+        }
+    }
+    
+    func resultAlert(tittle: String) {
+        let ac = UIAlertController(title: tittle, message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "Reset", style: .default, handler: { (_) in
+            self.resetBoard()
+        }))
+        self.present(ac, animated: true)
+    }
+    
+    func resetBoard() {
+        for button in board {
+            button.setTitle(nil, for: .normal)
+            button.isEnabled = true
+        }
+        if firstTurn == Turn.Nought {
+            firstTurn = Turn.Cross
+            turnLabel.text = CROSS
+        }
+        else if firstTurn == Turn.Cross {
+            firstTurn = Turn.Nought
+            turnLabel.text = NOUGHT
+        }
+        currentTurn = firstTurn
+    }
+    
+    func fullBoard() -> Bool {
+        for button in board
+        {
+            if button.title(for: .normal) == nil
+            {
+                return false
+            }
+        }
+        return true
     }
     
     func addToBoard(_ sender: UIButton)
@@ -46,18 +100,15 @@ class ViewController: UIViewController
                 currentTurn = Turn.Cross
                 turnLabel.text = CROSS
             }
-        }
-        else if sender.title(for: .normal) == nil
-        {
-            if (currentTurn==Turn.Cross)
+            else if sender.title(for: .normal) == nil
             {
                 sender.setTitle(CROSS, for: .normal)
                 currentTurn = Turn.Nought
                 turnLabel.text = NOUGHT
             }
+            sender.isEnabled = false
         }
     }
-    
 }
 
 
