574eddb09ec6bf489d160e8e4b3f208f63158f63
Oğulcan Aslan <batinsavran00@gmail.com>
2024-09-20 16:59:18 +0300

    Initial Tic-Tac-Toe Game Setup with Turn-Based Logic and UI
    Implemented the basic structure of a Tic-Tac-Toe game in Swift using Storyboard. Added an enum to manage player turns (Nought and Cross), created outlets for each button representing the game board, and implemented the logic to update the board when a button is tapped. The game now switches turns between players and updates the turn label accordingly.

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -1,19 +1,63 @@
-//
-//  ViewController.swift
-//  TicTacToe
-//
-//  Created by mobiroller on 20.09.2024.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class ViewController: UIViewController
+{
+    enum Turn
+    {
+        case Nought
+        case Cross
     }
-
-
+    
+    @IBOutlet weak var a1: UIButton!
+    @IBOutlet weak var a2: UIButton!
+    @IBOutlet weak var a3: UIButton!
+    @IBOutlet weak var b1: UIButton!
+    @IBOutlet weak var b2: UIButton!
+    @IBOutlet weak var b3: UIButton!
+    @IBOutlet weak var c1: UIButton!
+    @IBOutlet weak var c2: UIButton!
+    @IBOutlet weak var c3: UIButton!
+    @IBOutlet weak var turnLabel: UILabel!
+    
+    
+    var firstTurn = Turn.Cross
+    var currentTurn = Turn.Cross
+    
+    var NOUGHT = "O"
+    var CROSS = "X"
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func boardTapAction(_ sender: UIButton)
+    {
+        addToBoard(sender)
+    }
+    
+    func addToBoard(_ sender: UIButton)
+    {
+        if sender.title(for: .normal) == nil
+        {
+            if (currentTurn==Turn.Nought)
+            {
+                sender.setTitle(NOUGHT, for: .normal)
+                currentTurn = Turn.Cross
+                turnLabel.text = CROSS
+            }
+        }
+        else if sender.title(for: .normal) == nil
+        {
+            if (currentTurn==Turn.Cross)
+            {
+                sender.setTitle(CROSS, for: .normal)
+                currentTurn = Turn.Nought
+                turnLabel.text = NOUGHT
+            }
+        }
+    }
+    
 }
+
 
